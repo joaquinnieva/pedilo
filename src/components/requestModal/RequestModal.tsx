188233ef910b5bd383f750dd47a1dc506ac960d6
Franco Baigorria <74195715+FrancoBaig@users.noreply.github.com>
2024-01-15 10:26:29 -0300

    feat: implement roulette

---
+++ src/components/requestModal/RequestModal.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from '@nextui-org/react';
+import { useState } from 'react';
+import { Wheel } from 'react-custom-roulette';
+
+type Props = {
+  spinOptions: { option: string }[];
+};
+
+function RequestModal({ spinOptions }: Props) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+
+  const handleSpinClick = () => {
+    if (!mustSpin) {
+      const newPrizeNumber = Math.floor(Math.random() * spinOptions.length);
+      setPrizeNumber(newPrizeNumber);
+      setMustSpin(true);
+    }
+  };
+
+  if (spinOptions?.length === 0) return null;
+  return (
+    <>
+      <Button
+        color='warning'
+        onPress={onOpen}
+        disabled={spinOptions?.length === 0}
+      >
+        ¿Quién lo tiene que pedir?
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        className='bg-card'
+        size='lg'
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className='flex flex-col gap-1'>
+                ¿Quién lo tiene que pedir?
+              </ModalHeader>
+              <ModalBody>
+                <Wheel
+                  mustStartSpinning={mustSpin}
+                  prizeNumber={prizeNumber}
+                  data={spinOptions}
+                  textColors={['#fff']}
+                  backgroundColors={[
+                    '#df3428',
+                    '#ff6f3c',
+                    '#155263',
+                    '#ff9a3c',
+                    '#00bbf0',
+                    '#42b883',
+                    '#35495e',
+                    '#ff7e67',
+                  ]}
+                  onStopSpinning={() => {
+                    setMustSpin(false);
+                  }}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  color='secondary'
+                  variant='flat'
+                  onPress={() => {
+                    onClose();
+                  }}
+                >
+                  Cerrar
+                </Button>
+                <Button color='primary' onPress={handleSpinClick}>
+                  Girar
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+export default RequestModal;
--- src/components/sendRequest/SendRequest.tsx
+++ src/components/sendRequest/SendRequest.tsx
@@ -3,31 +3,42 @@ import { FormatCartToText } from '@/functions/GenerateMessage';
 import { Button } from '@nextui-org/react';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
+import { RequestModal } from '..';
 
 function SendRequest({ requests }: { requests: any }) {
   const [text, setText] = useState('');
 
   const previewText = () => {
     setText(FormatCartToText(requests, MessageGenerations.PREVIEW));
-    navigator.clipboard.writeText(FormatCartToText(requests, MessageGenerations.COPY));
+    navigator.clipboard.writeText(
+      FormatCartToText(requests, MessageGenerations.COPY)
+    );
     toast.success('Copiado al portapapeles');
   };
 
   const generateSent = () => {
-    window.open(`https://wa.me/+543517513954?text=${FormatCartToText(requests, MessageGenerations.WP)}`);
+    window.open(
+      `https://wa.me/+543517513954?text=${FormatCartToText(
+        requests,
+        MessageGenerations.WP
+      )}`
+    );
   };
 
   return (
-    <div className="w-full my-4 flex flex-col items-center">
-      <div className="flex gap-4">
-        <Button color="primary" onClick={previewText}>
+    <div className='w-full my-4 flex flex-col items-center'>
+      <div className='flex gap-4'>
+        <Button color='primary' onClick={previewText}>
           Ver PEDIDO
         </Button>
-        <Button color="primary" onClick={generateSent}>
+        <Button color='primary' onClick={generateSent}>
           PEDILO
         </Button>
+        <RequestModal
+          spinOptions={requests.map((req: any) => ({ option: req.name }))}
+        />
       </div>
-      <div className="mt-4" dangerouslySetInnerHTML={{ __html: text }}></div>
+      <div className='mt-4' dangerouslySetInnerHTML={{ __html: text }}></div>
     </div>
   );
 }
